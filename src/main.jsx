3ecb5674c99faee3de5259da4603342ddd7eadda
ealerto.ph@gmail.com <idel.aranilla@gmail.com>
2025-10-03 03:41:17 +0800

    removed vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./context/AppContext.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
-);
+  </BrowserRouter>,
+)
