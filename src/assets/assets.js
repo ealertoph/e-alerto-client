3dffcc5b3f2dde49274a0fca3328224956744213
ealerto.ph@gmail.com <idel.aranilla@gmail.com>
2025-10-03 02:27:09 +0800

    converted to webp

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,32 +1,17 @@
 import arrow_icon from "./arrow_icon.svg";
-import lock_icon from "./lock_icon.svg";
-import logo_white from "./logo-white.svg";
-import logo from "./logo.svg";
-import mail_icon from "./mail_icon.svg";
-import person_icon from "./person_icon.svg";
-import hand_wave from "./hand_wave.png";
-import header_img from "./header_img.png";
-import header_img1 from "./header_img1.png";
-import header_img2 from "./header_img2.png";
+import logo_white from "./logo-white.webp";
+import logo from "./logo.webp";
 import hero_phone from "./hero-phone.webp";
-import pothole from "./pothole.jpg";
-import cracks from "./cracks.jpg";
-import qcde_logo from "./qcde-logo.svg";
+import pothole from "./pothole.webp";
+import cracks from "./cracks.webp";
+import qcde_logo from "./qcde-logo.webp";
 import qc_monument from "./qc-monument.png";
 import digibridge_solutions from "./digibridge_solutions.png";
 import login_bg from "./login-bg.jpg";
-import hero_bg from "./hero-bg.jpg";
 
 export const assets = {
   arrow_icon,
-  lock_icon,
   logo,
-  mail_icon,
-  person_icon,
-  hand_wave,
-  header_img,
-  header_img1,
-  header_img2,
   logo_white,
   hero_phone,
   pothole,
@@ -35,5 +20,4 @@ export const assets = {
   qc_monument,
   digibridge_solutions,
   login_bg,
-  hero_bg,
 };
